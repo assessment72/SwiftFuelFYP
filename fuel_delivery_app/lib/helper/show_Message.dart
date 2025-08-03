a939b594626d33831d064a62c1cb5b9efbe088af
assessment72 <moh734953951@gmail.com>
2025-08-04 02:01:04 +0300

    Create show_Message.dart

---
+++ fuel_delivery_app/lib/helper/show_Message.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void showMessage(BuildContext context, String message) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        backgroundColor: Colors.deepPurple,
+        content: Padding(
+          padding: const EdgeInsets.all(6),
+          child: Text(
+            message,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        actions: [
+          Center(
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('OK'),
+            ),
+          )
+        ],
+      );
+    },
+  );
+}
