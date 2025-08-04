d827b2377d7d8ad286c4b4690e43b180ce787561
assessment72 <moh734953951@gmail.com>
2025-08-04 03:59:19 +0300

    Update main.dart

--- fuel_delivery_app/lib/main.dart
+++ fuel_delivery_app/lib/main.dart
@@ -1,39 +1,53 @@
 /// Author: Fahad Riaz
 /// Description: This is the main entry point of the SwiftFuel app. It initializes Firebase,
 /// sets up Stripe payment integration, and defines the main routes for navigation
-/// across the app such as login, registration, home, and fuel ordering screens.
+/// across the app such as login, registration, home, notifications, orders, and account screens.
 /// This file ensures the app is properly bootstrapped with necessary configurations.
 
-
-
 import 'package:flutter/material.dart';
-import 'package:fuel_delivery_app/screens/fuelordering_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/register_screen.dart';
-import 'screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
+
+// Screens
+import 'package:fuel_delivery_app/screens/login_screen.dart';
+import 'package:fuel_delivery_app/screens/register_screen.dart';
+import 'package:fuel_delivery_app/screens/home_screen.dart';
+import 'package:fuel_delivery_app/screens/fuelordering_screen.dart';
+import 'package:fuel_delivery_app/screens/notifications_screen.dart';
+import 'package:fuel_delivery_app/screens/pastorders_screen.dart';
+import 'package:fuel_delivery_app/screens/user_profile_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   Stripe.publishableKey = "pk_test_51QvWmxLdbeQ0UiZd1jqwZ2vPWtDNJqVAi3advQvTrixYOZYDxmBVDTpdxIaN9C01HddNAbTeEtrsMMOcLTvjlygO00dGcWbyqv";
 
-  runApp(MyApp());
+  runApp(const SwiftFuelApp());
 }
 
-class MyApp extends StatelessWidget {
+class SwiftFuelApp extends StatelessWidget {
+  const SwiftFuelApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Authentication',
+      title: 'SwiftFuel',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+      ),
       initialRoute: '/login',
       routes: {
-        '/register': (context) => RegisterScreen(),
-        '/login': (context) => LoginScreen(),
-        '/home': (context) => HomeScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
+        '/home': (context) => const HomeScreen(),
         '/orderfuel': (context) => FuelOrderingScreen(),
-
+        '/notifications': (context) => const NotificationsScreen(),
+        '/orders': (context) => PastOrdersScreen(),
+        '/account': (context) => const UserProfileScreen(),
       },
     );
   }
